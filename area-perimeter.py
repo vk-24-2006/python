d44749df543db3c8b11f83660d46a1ffaee82246
vk-24-2006 <vivek.krishnan2006@gmail.com>
2025-07-07 15:44:12 +0530

    Add files via upload

---
+++ area-perimeter.py
@@ -0,0 +1,5 @@
+length = int(input("Enter the length of rectangle : "))
+breadth = int(input("Enter the breadth of rectengle : "))
+area = length * breadth
+perimeter = 2 * (length + breadth)
+print("The are of rectangle is ",area,"and perimeter is ",perimeter)
