d064a942969183651f464940ba49fbe652ff3508
vk-24-2006 <vivek.krishnan2006@gmail.com>
2025-07-07 15:44:30 +0530

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,4 @@
+temp = float(input("Enter the temperature in celsius : "))
+far = (temp * 9/5)+ 32
+kel = temp + 273.15
+print("The temperature in celsius is ",temp,"Which is ",far,"farenheit and ",kel,"Kelvin")
