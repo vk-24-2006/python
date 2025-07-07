587d82ed690c4b7371622e7c8331c67a2f4df23a
vk-24-2006 <vivek.krishnan2006@gmail.com>
2025-07-07 15:45:21 +0530

    Add files via upload

---
+++ largest_of_three.py
@@ -0,0 +1,5 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+second_largest = sorted([a, b, c])[1]
+print("The second largest number is:", second_largest)
