449486d17bbc92d9ab890fa153d605d062cb7e78
vk-24-2006 <vivek.krishnan2006@gmail.com>
2025-07-15 11:37:21 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,6 @@
+def add(x,y): return x + y
+def subtract(x,y): return x - y
+def multiply(x,y): return x * y
+def divide(x,y): return x / y if y != 0 else "Error"
+
+#modular function remaining part in main.py
