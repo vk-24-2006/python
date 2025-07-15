231495a44f744c63accd0c77cf938d5e89e4a8f3
vk-24-2006 <vivek.krishnan2006@gmail.com>
2025-07-15 11:37:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import calculator
+a = int(input("Enter a :"))
+b = int(input("Enter b :"))
+print("Sum : ",calculator.add(a, b))
+print("Difference : ",calculator.subtract(a, b))
+print("Product : ",calculator.multiply(a, b))
+print("Quotient : ",calculator.divide(a, b))
